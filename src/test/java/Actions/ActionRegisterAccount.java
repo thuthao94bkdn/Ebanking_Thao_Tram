015395988775acc2ead35c6cba0a60f297243b78
thuthao94bkdn <thuthao94bkdn@gmail.com>
2019-01-09 18:06:20 +0700

    Test2

--- src/test/java/Actions/ActionRegisterAccount.java
+++ src/test/java/Actions/ActionRegisterAccount.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ActionRegisterAccount {
-    public static void clickoncreateaccount(WebDriver driver){
+    public static void clickOnCreateAccount(WebDriver driver){
         driver.findElement(By.xpath(PageRegisterAccount.btn_CreateAccount)).click();
     }
 }
